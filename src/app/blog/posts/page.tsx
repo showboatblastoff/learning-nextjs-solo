a4f6c709b9deb1866305162bc1087a9c91d680ec
showboatblastoff <jfzangrilli@gmail.com>
2025-05-29 22:13:00 -0500

    only logged in users see New + button

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/components/button';
 import Post from '@/app/ui/components/posts/Post';
 import { fetchPosts } from '@/app/lib/data';
+import { auth } from '../../../../auth.config';
 
 // Force dynamic rendering since we're using noStore()
 export const dynamic = 'force-dynamic';
@@ -9,15 +10,15 @@ export const dynamic = 'force-dynamic';
 export default async function Page() {
   // This will trigger loading.tsx while data is being fetched
   const posts = await fetchPosts();
-
+  const session = await auth();
   return (
     <>
       <h1>Posts</h1>
-      <Link href="/blog/post/insert">
+      {session?.user && <Link href="/blog/post/insert">
         <Button className="outline outline-1 border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">
           New +
         </Button>
-      </Link>
+      </Link>}
       {posts?.map((post) => (
         <Post key={post.id} {...post} />
       ))}
